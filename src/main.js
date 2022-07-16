48bb0bd5e58db647ecf91fac64f52d98fb06c30f
Nakoya Wilson <wilson.nakoya@gmail.com>
2022-07-16 13:58:43 -0400

    Add styles for mobile view

--- src/main.js
+++ src/main.js
@@ -1,1 +1,48 @@
 import "./styles.css";
+
+const prevBtn = document.querySelector("#prev");
+const nextBtn = document.querySelector("#next");
+const slidesContainer = document.querySelector("#slides-container");
+const slides = document.querySelectorAll(".slide");
+let currentIndex = 2;
+let isMoving = false;
+
+slidesContainer.style.transform = `translateX(-${
+  currentIndex * slides[0].clientWidth
+}px)`;
+
+const handleBtnClick = (e) => {
+  if (isMoving) return;
+  isMoving = true;
+  e.currentTarget.id === "prev" ? currentIndex-- : currentIndex++;
+  slidesContainer.style.transform = `translateX(-${
+    currentIndex * slides[0].clientWidth
+  }px)`;
+  removeDisabledAttribute([prevBtn, nextBtn]);
+  currentIndex === 0 && addDisabledAttribute([prevBtn]);
+};
+
+const removeDisabledAttribute = (els) =>
+  els.forEach((el) => el.removeAttribute("disabled"));
+const addDisabledAttribute = (els) =>
+  els.forEach((el) => el.setAttribute("disabled", "true"));
+
+prevBtn.addEventListener("click", handleBtnClick);
+nextBtn.addEventListener("click", handleBtnClick);
+
+slidesContainer.addEventListener("transitionend", () => (isMoving = false));
+
+document
+  .querySelectorAll(".slide img")
+  .forEach((img) => (img.ondragstart = () => false));
+
+const slideObserver = new IntersectionObserver(
+  (slide) => {
+    if (slide[0].isIntersecting) {
+      addDisabledAttribute([nextBtn]);
+    }
+  },
+  { threshold: 0.75 }
+);
+
+slideObserver.observe(slides[slides.length - 1]);
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -17,6 +17,51 @@ module.exports = {
         "project-cyan": "hsl(172, 46%, 57%)",
         "dark-purple": "hsl(314, 46%, 23%)",
       },
+      borderRadius: {
+        ten: "0.625rem",
+        "twenty-eight": "1.75rem",
+      },
+      lineHeight: {
+        "twenty-six": "1.625rem",
+        thirty: "1.875rems",
+        "thirty-three": "2.0625rem",
+        "forty-five": "2.8125rem",
+      },
+      width: {
+        "mobile-avatar": "300px",
+        "mobile-container": "min(91.46667%, 343px)",
+        "mobile-paragraph": "min(100%, 285px)",
+        "mobile-slide": "min(100%, 270px)",
+      },
+      height: {
+        "one-five-eight": "9.875rem",
+        "one-eight-two": "11.375rem",
+        "three-six-four": "22.75rem",
+      },
+      fontSize: {
+        "twenty-six": "1.625rem",
+      },
+      margin: {
+        "one-hundred": "6.25rem",
+        thirty: "1.875rem",
+      },
+      backgroundImage: {
+        "apps-pattern": "url('./src/assets/pattern-apps.svg')",
+        "graphic-design-pattern":
+          "url('./src/assets/pattern-graphic-design.svg')",
+        "illustrations-pattern":
+          "url('./src/assets/pattern-illustrations.svg')",
+        "motion-graphics-pattern":
+          "url('./src/assets/pattern-motion-graphics.svg')",
+        "photography-pattern": "url('./src/assets/pattern-photography.svg')",
+        "ui-ux-pattern": "url('./src/assets/pattern-ui-ux.svg')",
+      },
+      backgroundPosition: {
+        "right-top": "right 1.5rem top 1.5rem",
+      },
+      gap: {
+        slider: "1.875rem",
+      },
     },
   },
   plugins: [],
