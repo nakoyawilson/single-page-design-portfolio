90f4133c3c8839949e86924181cf56a34bf3689f
Nakoya Wilson <wilson.nakoya@gmail.com>
2022-07-16 11:20:02 -0400

    Add fonts and custom colors

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,22 @@
 module.exports = {
   content: ["./index.html", "./src/main.js"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        primary: "Plus Jakarta Sans",
+      },
+      colors: {
+        "project-black": "hsl(0, 0%, 1%)",
+        "medium-brown": "hsl(30, 5%, 45%)",
+        "light-cream": "hsl(28, 100%, 97%)",
+        "galactic-blue": "hsl(252, 66%, 62%)",
+        "summer-yellow": "hsl(28, 89%, 67%)",
+        "project-pink": "hsl(0, 79%, 79%)",
+        "light-red": "hsl(7, 77%, 66%)",
+        "project-cyan": "hsl(172, 46%, 57%)",
+        "dark-purple": "hsl(314, 46%, 23%)",
+      },
+    },
   },
   plugins: [],
 };
